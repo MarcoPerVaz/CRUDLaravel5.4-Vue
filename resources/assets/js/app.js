5e1a38059bd9c1ca2e76fb398948602c50a3c4df
Curso Git <marcooperdomo@gmail.com>
2019-08-01 15:03:34 -0500

    Estructuración de controlador, migración, vistas y rutas

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,18 +1,4 @@
-//var urlUsers = 'https://randomuser.me/api/?results=10'; //Sitio web https://randomuser.me
-var urlUsers = 'https://jsonplaceholder.typicode.com/users'; //Sitio web https://jsonplaceholder.typicode.com/users
 new Vue({
   el: '#main',
-    created: function(){
-      this.getUsers();
-    },
-    data: {
-      lists: [],
-    },
-    methods: {
-      getUsers: function(){
-        axios.get(urlUsers).then(response => {
-          this.lists = response.data;
-        });
-      }
-    }
+  
 });
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,22 +19,9 @@
     <body>
         <div id="main">
             <div class="container">
-                <div class="row">
-                    <div class="col-sm-5">
-                        <h1>Lista VueJs - Ajax con Axios</h1>
-                        <ul class="list-group">
-                            <li class="list-group-item" v-for="item in lists">
-                                @{{ item.name }}
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-sm-7">
-                        <h1>JSON</h1>
-                        <pre>
-                            @{{ $data }}
-                        </pre>
-                    </div>
-                </div>
+
+                @yield('content')
+                
             </div>
         </div>
 
