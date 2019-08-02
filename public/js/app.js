fc2bab1b154132c080c87d4bbbc0fc6b3ed9d56c
Curso Git <marcooperdomo@gmail.com>
2019-08-01 23:02:12 -0500

    Eliminar Tareas con VueJs

--- public/js/app.js
+++ public/js/app.js
@@ -13646,13 +13646,19 @@ new Vue({
     this.getKeeps();
   },
   data: {
-    keeps: [],
+    keeps: []
   },
   methods: {
     getKeeps: function(){
       var urlKeeps = 'tasks';
       axios.get(urlKeeps ).then(response => {
         this.keeps = response.data;
+      });
+    },
+    deleteKeep: function(keep){
+      var url = 'tasks/' + keep.id;
+      axios.delete(url).then(response => {
+        this.getKeeps();
       });
     }
   }
