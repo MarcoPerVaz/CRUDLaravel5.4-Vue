e0aee385c71a8222f9377eed689969b9ac5d2714
Curso Git <marcooperdomo@gmail.com>
2019-08-02 09:54:29 -0500

    Notificaciones con TOASTR

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,8 +15,18 @@ new Vue({
     },
     deleteKeep: function(keep){
       var url = 'tasks/' + keep.id;
-      axios.delete(url).then(response => {
-        this.getKeeps();
+      axios.delete(url).then(response => { //Se eliminan las tareas
+        this.getKeeps(); //Se listan las tareas
+        // Las 2 propiedades de toastr.options funcionan.
+        // Forma 1
+        toastr.options = {
+          "closeButton": true,
+          "progressBar": true,
+        }
+        // Forma 2
+        // toastr.options.closeButton = true;
+        // toastr.options.progressBar = true;
+        toastr.success('Eliminado correctamente');//Se muestra el mensaje
       });
     }
   }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,13 +7,7 @@
 
         <title>Laravel 5.4 - Vue</title>
         {{-- Styles --}}
-        {{-- Boostrap 3.3.7 --}}
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
-        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     </head>
     <body>
