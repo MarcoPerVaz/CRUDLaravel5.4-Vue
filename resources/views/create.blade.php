3f89f73afcd4c54df6c296a052d962b16f094e7a
Curso Git <marcooperdomo@gmail.com>
2019-08-02 10:59:55 -0500

    Implementando Modal para el Form Crear

---
+++ resources/views/create.blade.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="create">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">
+          <span>&times;</span>
+        </button>
+        <h4>Nueva tarea</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <input type="submit" value="Guardar" class="btn btn-primary">
+      </div>
+    </div>
+  </div>
+</div>
